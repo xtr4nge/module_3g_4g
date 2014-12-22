ddc77284a3a572ef5a35bfa07a0d498ce3bb7dc3
xtr4nge <xtr4nge@gmail.com>
2014-12-22 22:44:25 +0000

    mod_isup has been fixed

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="3g_4g";
-$mod_version="1.1";
+$mod_version="1.2";
 $mod_path="/usr/share/fruitywifi/www/modules/$mod_name";
 $mod_logs="$log_path/$mod_name.log"; 
 $mod_logs_history="$mod_path/includes/logs/";
@@ -14,6 +14,7 @@ $ss_mode = "mode_mobile";
 
 # EXEC
 $bin_danger = "/usr/share/fruitywifi/bin/danger";
+$bin_sudo = "/usr/bin/sudo";
 $bin_ifconfig = "/sbin/ifconfig";
 $bin_iwlist = "/sbin/iwlist";
 $bin_sh = "/bin/sh";
@@ -30,5 +31,6 @@ $bin_sleep = "/bin/sleep";
 $bin_nmcli = "/usr/share/fruitywifi/www/modules/nmcli/includes/NetworkManager/cli/src/nmcli";
 
 # ISUP
-$mod_isup="$bin_danger \"$bin_nmcli -n d | grep -iEe 'gsm.+ connected'\"";
+//$mod_isup="$bin_danger \"$bin_nmcli -n d | grep -iEe 'gsm.+ connected'\"";
+$mod_isup="$bin_sudo $bin_nmcli -n d | grep -iEe 'gsm.+ connected'";
 ?>
